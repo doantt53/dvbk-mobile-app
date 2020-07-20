19331c5604213c9de8183c5a80b351d4797a921d
doantt <doantt53@gmail.com>
2020-07-20 11:14:34 +0700

    Update firebase

--- lib/NetworkUtil.dart
+++ lib/NetworkUtil.dart
@@ -20,7 +20,8 @@ class NetworkUtil {
       if (statusCode < 200 || statusCode > 400 || json == null) {
         throw new Exception("Error while fetching data");
       }
-      return _decoder.convert(res);
+//      return _decoder.convert(res);
+      return res;
     });
   }
 
