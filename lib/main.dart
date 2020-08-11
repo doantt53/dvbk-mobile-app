935443288e01c737fed9475a8d34be79ed7233aa
Tranngocquy <gemq289x@gmail.com>
2020-08-11 08:06:54 +0700

    u

--- lib/main.dart
+++ lib/main.dart
@@ -120,7 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Text('Ok'),
                 onPressed: () {
                   //Navigator.of(context).pop();
-                  SystemNavigator.pop();
+                  SystemNavigator.pop(); // Close cả app ra ngoài
                 },
               )
             ],
